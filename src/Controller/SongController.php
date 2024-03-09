95994750710c9aef5d230ffc6641d148e097401b
Oussema Bouzidi <bouzidioussema16@gmail.com>
2024-03-09 18:16:41 +0100

    first commit

---
+++ src/Controller/SongController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use Psr\Log\LoggerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SongController extends AbstractController
+{
+    #[Route('/api/song/{id<\d+>}', methods: ['GET'])]
+    public function getSong(int $id, LoggerInterface $logger): Response
+    {
+        $song = [
+            'id' => $id,
+            'name' => 'Waterfalls',
+            'url' => 'https://symfonycasts.s3.amazonaws.com/sample.mp3',
+        ];
+
+        $logger->info('Returning api responce for song with id : {song}', [
+            'song' => $id,
+        ]);
+
+        return $this->json($song);
+    }
+}
